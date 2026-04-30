e6db8f73746b972b12d32517d34c465232631b7c
achernandezh <152939117+achernandezh@users.noreply.github.com>
2026-04-29 22:16:35 -0400

    Update script-2.js

--- script-2.js
+++ script-2.js
@@ -1,3 +1,6 @@
 function btn1(){
     window.open("https://maps.app.goo.gl/J5xpn3VsUhk26M3q9");
   }
+  document.getElementById("disney-text").style.fontFamily = "DisneyFont, sans-serif";
+  document.getElementById("moana-text").style.fontFamily = "MoanaFont";
+  document.getElementById("angelina-text").style.fontFamily = "AngelinaFont";
